b171737b1e4c3cae035d184ab7ed8f4d1b6b6430
Mateusz Szczęch <szczechmatt@gmail.com>
2020-02-03 16:54:07 +0100

    add items to local storage and stringify it

--- src/scripts/cart/cart.js
+++ src/scripts/cart/cart.js
@@ -1,7 +1,16 @@
+import products from './products/products';
+
 const cartBtn = document.querySelectorAll('.cart__buy-btn');
 const counter = document.querySelector('.cart__counter');
 
-const cartCounterToStorage = () => {
+const passTheProduct = product => {
+  const cartItems = {
+    [product.id]: product,
+  };
+  localStorage.setItem('InCart', JSON.stringify(cartItems));
+};
+
+const cartCounterToStorage = product => {
   let cartValue = localStorage.getItem('Cart Value');
   cartValue = parseInt(cartValue, 10);
   if (!cartValue) {
@@ -11,6 +20,7 @@ const cartCounterToStorage = () => {
     localStorage.setItem('Cart Value', cartValue + 1);
     counter.innerText = cartValue + 1;
   }
+  passTheProduct(product);
 };
 
 const displayCounterToCart = () => {
@@ -18,7 +28,11 @@ const displayCounterToCart = () => {
 };
 
 cartBtn.forEach(btn => {
-  btn.addEventListener('click', cartCounterToStorage);
+  const dataId = btn.dataset.id;
+  const eachProduct = products[dataId];
+  btn.addEventListener('click', () => {
+    cartCounterToStorage(eachProduct);
+  });
 });
 
 displayCounterToCart();
---
+++ src/scripts/cart/products/products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    name: 'Corpo template',
+    id: 'corpo',
+    price: 10,
+    inCart: 0,
+  },
+  {
+    name: 'Color template',
+    id: 'color',
+    price: 20,
+    inCart: 0,
+  },
+  {
+    name: 'Family template',
+    id: 'family',
+    price: 25,
+    inCart: 0,
+  },
+];
+export default products;
