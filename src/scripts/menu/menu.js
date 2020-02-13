14afa55398985b61c778b38276614553ae711e53
Mateusz Szczęch <szczechmatt@gmail.com>
2020-02-13 11:35:40 +0100

    fix timeout error

--- src/scripts/menu/menu.js
+++ src/scripts/menu/menu.js
@@ -2,19 +2,24 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 const headerNavigation = document.querySelector('.header__navigation');
 const header = document.querySelector('.header');
-
+const order = document.querySelector('.order');
 const handleClick = () => {
   hamburger.classList.toggle('hamburger--active');
   nav.classList.toggle('nav--active');
 };
 
 const stickyNavigation = () => {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    headerNavigation.classList.add('header__navigation--active');
-    header.style.paddingTop = `${headerNavigation.offsetHeight - 20}px`;
-  } else {
-    header.style.paddingTop = 0;
-    headerNavigation.classList.remove('header__navigation--active');
+  if (!order) {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      headerNavigation.classList.add('header__navigation--active');
+      header.style.paddingTop = `${headerNavigation.offsetHeight - 20}px`;
+    } else {
+      header.style.paddingTop = 0;
+      headerNavigation.classList.remove('header__navigation--active');
+    }
   }
 };
 
